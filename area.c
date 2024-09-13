d85aeacb063d29579dc329d240cc51ea58eebe7e
ionmaniano <odiwuorian@gmail.com>
2024-09-13 18:23:58 +0300

    first commit

---
+++ area.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+    
+    int length,width,area;//declaration and initialization
+    
+    printf("Enter length:");
+    scanf("%d",&length);
+    
+    printf("Enter width:");
+    scanf("%d",&width);
+    
+    area = length * width;
+    
+    
+    printf("The area is: %d" ,area);
+    
+    
+    
+
+    
+}
